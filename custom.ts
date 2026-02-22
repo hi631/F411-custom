a2b8accaf01c4d8b658496bfda87f24f1ed9aef2
hi631 <hi631@ac.auone-net.jp>
2026-02-22 16:11:27 +0900

    更新 main.blocks, custom.ts, f411.cpp

--- custom.ts
+++ custom.ts
@@ -3,6 +3,24 @@ namespace myExtension {
     /**
      * STM32F411で入出力するブロック
      */
+    //% block="f411_gpioa with %value %counr"
+    //% shim=myExtension::f411_gpioa
+    export function f411_gpioa(value: number, count: number): void {
+        // シミュレータ用のフォールバック処理（実機ではshimが呼ばれる）
+        return;
+    }
+    /**
+     * STM32F411で入出力するブロック
+     */
+    //% block="f411_gpiob with %value %counr"
+    //% shim=myExtension::f411_gpiob
+    export function f411_gpiob(value: number, count: number): void {
+        // シミュレータ用のフォールバック処理（実機ではshimが呼ばれる）
+        return;
+    }
+    /**
+     * STM32F411で入出力するブロック
+     */
     //% block="f411_gpioc with %value %counr"
     //% shim=myExtension::f411_gpioc
     export function f411_gpioc(value: number, count: number): void {
--- f411.cpp
+++ f411.cpp
@@ -2,6 +2,9 @@
 #include "stm32f4xx_hal.h"
 
 namespace myExtension {
+    int init_gpioa = 0;
+    int init_gpiob = 0;
+    int init_gpioc = 0;
     void init_gpio(GPIO_TypeDef *GPIOx, int gpin){
 	  if(GPIOx==GPIOA) __HAL_RCC_GPIOA_CLK_ENABLE();
 	  if(GPIOx==GPIOB) __HAL_RCC_GPIOB_CLK_ENABLE();
@@ -13,22 +16,26 @@ namespace myExtension {
 	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	  HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
 }
-
     //%
-    void f411_gpioc(int value, int count) {
-
+    void f411_gpioa(int value, int count) {
         int gpin=1 << value;
-        init_gpio(GPIOC, gpin);
+        if(init_gpioa==0) { init_gpio(GPIOA, gpin); init_gpioa = 1;}
         if(count==0) HAL_GPIO_WritePin(GPIOC, gpin, GPIO_PIN_SET);    // off
         else         HAL_GPIO_WritePin(GPIOC, gpin, GPIO_PIN_RESET);  // on
-        //int tc;
-        //int lc=count;
-        //while(lc--) {
-        //    HAL_GPIO_WritePin(GPIOC, gpin, GPIO_PIN_SET);    // off
-        //    tc=0x200000; while(tc--) { __NOP();  }
-        //    HAL_GPIO_WritePin(GPIOC, gpin, GPIO_PIN_RESET);  // on
-        //    tc=0x200000; while(tc--) { __NOP();  }
-        //}
+    }
+    //%
+    void f411_gpiob(int value, int count) {
+        int gpin=1 << value;
+        if(init_gpiob==0) { init_gpio(GPIOB, gpin); init_gpiob = 1;}
+        if(count==0) HAL_GPIO_WritePin(GPIOC, gpin, GPIO_PIN_SET);    // off
+        else         HAL_GPIO_WritePin(GPIOC, gpin, GPIO_PIN_RESET);  // on
+    }
+    //%
+    void f411_gpioc(int value, int count) {
+        int gpin=1 << value;
+        if(init_gpioc==0) { init_gpio(GPIOC, gpin); init_gpioc = 1;}
+        if(count==0) HAL_GPIO_WritePin(GPIOC, gpin, GPIO_PIN_SET);    // off
+        else         HAL_GPIO_WritePin(GPIOC, gpin, GPIO_PIN_RESET);  // on
     }
 }
 
